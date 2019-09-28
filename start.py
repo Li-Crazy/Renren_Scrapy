df94df28a31a81e36b030c852baa3f231dad5bdf
Li-Crazy <52807717+Li-Crazy@users.noreply.github.com>
2019-09-28 17:15:50 +0800

    first commit

---
+++ start.py
@@ -0,0 +1,9 @@
+'''
+-*- coding: utf-8 -*-
+@Author  : LiZhichao
+@Time    : 2019/6/5 10:14
+@Software: PyCharm
+@File    : start.py
+'''
+from scrapy import cmdline
+cmdline.execute("scrapy crawl renren".split())
